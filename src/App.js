17a11929cd21c32ab9a39d6d206f0d88bb1ac498
kimdongho <ehdrh321@gmail.com>
2022-03-17 16:34:37 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import faker from '@faker-js/faker';
 import faker_ko from '@faker-js/faker/locale/ko';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import UserCard from './component/UserCard'
+
 function App() {
   const userDatas = [];
 
@@ -19,29 +16,8 @@ function App() {
     })
 }
 
-  const userCards = userDatas.map((userData, index) =>{ 
-    return <Card sx={{ maxWidth: 345 }}>
-    <CardActionArea>
-      <CardMedia
-
-        component="img"
-        height="140"
-        image={userData.avatar}
-        alt="green iguana"
-
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {userData.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {userData.email}<br></br>
-          {userData.jobTitle}<br></br>
-          {userData.phonNo}
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-  </Card>
+  const userCards = userDatas.map((userData, idx) =>{ 
+    return <UserCard userData={userData} idx={idx} />
   });
 
   
