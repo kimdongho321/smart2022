7e0a3d3a0d9175bd4ea5029f32b9aae82ffa6a28
kimdongho <ehdrh321@gmail.com>
2022-03-17 14:56:28 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
+import faker, { Faker } from '@faker-js/faker';
+import faker_ko from '@faker-js/faker/locale/ko';
 function App() {
+  const userDatas = [];
+
+  while(userDatas.length < 50){
+    userDatas.push({
+      avatar: faker.image.avatar() ,
+      name: `${faker_ko.name.lastName()} ${faker_ko.name.firstName()}`,
+      email: faker.internet.email()  ,
+      jobTitle: faker.name.jobTitle() ,
+      phonNo: faker_ko.phone.phoneNumber()
+    })
+}
+
+  const userCards = userDatas.map((userData) =>{ 
+    return <>
+      <h4>{userData.jobTitle}</h4>
+        <img src={userData.avatar} alt="사용자 프로필용 아바타"></img>
+        <h5>{userData.name}</h5>
+        {userData.email} <br />
+        {userData.phonNo} 
+    </>
+  });
+
+  const userData = {
+    avatar: faker.image.avatar() ,
+    name: `${faker_ko.name.lastName()} ${faker_ko.name.firstName()}`,
+    email: faker.internet.email()  ,
+    jobTitle: faker.name.jobTitle() ,
+    phonNo: faker_ko.phone.phoneNumber()
+  }
+
+  console.log(userDatas)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {userCards}
     </div>
   );
 }
