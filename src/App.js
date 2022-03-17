d0f1191a50d1d7480c10b29b60756c646b4aa317
kimdongho <ehdrh321@gmail.com>
2022-03-17 15:35:33 +0900

    card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import faker, { Faker } from '@faker-js/faker';
 import faker_ko from '@faker-js/faker/locale/ko';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 function App() {
   const userDatas = [];
 
@@ -14,14 +19,41 @@ function App() {
     })
 }
 
-  const userCards = userDatas.map((userData) =>{ 
-    return <>
+  const userCards = userDatas.map((userData, index) =>{ 
+    return <Card sx={{ maxWidth: 345 }}>
+    <CardActionArea>
+      <CardMedia
+
+        component="img"
+        height="140"
+        image={userData.avatar}
+        alt="green iguana"
+
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {userData.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {userData.email}<br></br>
+          {userData.jobTitle}<br></br>
+          {userData.phonNo}
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+  </Card>
+    
+    
+    
+    
+    
+    {/* <div key={index}>
       <h4>{userData.jobTitle}</h4>
         <img src={userData.avatar} alt="사용자 프로필용 아바타"></img>
         <h5>{userData.name}</h5>
         {userData.email} <br />
         {userData.phonNo} 
-    </>
+    </div> */}
   });
 
   const userData = {
@@ -31,8 +63,6 @@ function App() {
     jobTitle: faker.name.jobTitle() ,
     phonNo: faker_ko.phone.phoneNumber()
   }
-
-  console.log(userDatas)
   
   return (
     <div className="App">
